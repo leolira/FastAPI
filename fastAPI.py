5c51aac0edc806685c6c98c3199de0f0cb2a1dc5
leolira <leonardolira@gmail.com>
2024-08-07 11:23:44 -0300

    idgen created, base data created

--- fastAPI.py
+++ fastAPI.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+app = FastAPI()
+
+class Users(BaseModel):
+    id: int
+    name: str
+    lastname: str
+    age: int
+
+def id_generator():
+    return max(user['id'] for user in users)+1
+
+users = [
+    Users(id=1, name="Leonardo", lastname="Amorim", age=42),
+    Users(id=2, name="Luana", lastname="Menezes", age=35)
+]
